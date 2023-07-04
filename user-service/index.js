ca055ffeec34c9a664631161a44d2cddf885fe84
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-07-04 15:26:34 +0530

    refactor: Add new step to get github short sha

--- user-service/index.js
+++ user-service/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from User Service! ");
+  res.send("Hello from User Service!");
 });
 
 app.listen(port, () => {
